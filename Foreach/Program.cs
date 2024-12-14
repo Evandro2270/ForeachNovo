99b49ef831764c0a03a7e85ad678bd26e2c0c522
Evandro2270 <evandro535@gmail.com>
2024-12-14 17:03:36 -0300

    Adicionar arquivos de projeto.

---
+++ Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                string[] pessoas = new string[] {"Maria", "Carlos", "Marcela", "Ana" };
+
+                foreach (string nPesssoas in pessoas) {
+                    Console.WriteLine(nPesssoas);
+                }
+            }
+        }
+    }
+}
